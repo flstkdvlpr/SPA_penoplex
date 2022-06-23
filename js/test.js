477ef20d8dd101359304df6ae3c6dab7babf5355
Artem <sartvas.ne@yahoo.com>
2022-06-23 08:16:52 +0300

    подключен JS2

--- js/test.js
+++ js/test.js
@@ -1,2 +1,20 @@
 const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+
 console.log(numberOfFilms);
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const a = prompt("Один из просмотренных последних фильмов?", "");
+const b = prompt("Один из просмотренных последних фильмов?", "");
+
+personalMovieDB.movies.a = prompt("Как его оцените?", "");
+personalMovieDB.movies.b = prompt("Как его оцените?", "");
+
+console.log(personalMovieDB.movies);
