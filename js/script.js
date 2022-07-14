c8d55f711fa749122658a8202c68e52aff8831b1
Artem <sartvas.ne@yahoo.com>
2022-07-14 21:46:35 +0300

    Пишем скрипт на карточки

--- js/script.js
+++ js/script.js
@@ -1,19 +1,2 @@
-// Место для второй задачи
-function secondTask() {
-    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-    const data = [5, 10, 'Shopping', 20, 'Homework'];
-
-    for (let i = 0; i < data.length; i++) {
-        if (typeof(i) === Number){
-            data[i] = i*2; 
-        } else {
-            data[i] = i + '- done';
-        }
-    }
-    
-    // Не трогаем
-    return data;
-}
-
-
-console.log(secondTask());
+const cards = document.querySelectorAll(".card-text");
+console.log(cards[0].outerText == "Теплоизоляция внешних и внутренних ограждающих конструкций.");
