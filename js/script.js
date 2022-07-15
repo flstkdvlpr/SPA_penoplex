1c773c8936aa6ff321b4d44ad72f3706f93af963
Artem <sartvas.ne@yahoo.com>
2022-07-15 10:44:45 +0300

    Реализована функция сокрытия блоков с характеристиками

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
-const cards = document.querySelectorAll(".card-text");
-console.log(cards[0].outerText == "Теплоизоляция внешних и внутренних ограждающих конструкций.");
+window.addEventListener('DOMContentLoaded', ()=>{
+    
+    const links = document.querySelectorAll(".card-link"),
+          cardBody = document.querySelectorAll('.card-body'),
+          card = document.querySelectorAll('.card');
+
+          console.log(links[0].parentNode.parentNode);
+
+          function hideSpecification(){
+            card.forEach(item =>{
+                if (item.dataset.value == 'spectitle'){
+                    item.classList.add('hide');
+                }
+                
+            });
+          }
+    
+    hideSpecification();
+      
+});
+
