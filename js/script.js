9fae670ebf25a6584a25ffcdd1821e691ffb771b
Artem <sartvas.ne@yahoo.com>
2022-07-18 16:45:32 +0300

    Работа над показом блока с характеристиками

--- js/script.js
+++ js/script.js
@@ -1,38 +1,34 @@
 window.addEventListener('DOMContentLoaded', ()=>{
 
     const links = document.querySelectorAll(".card-link"),
-          cardBody = document.querySelectorAll('.card-body'),
-          card = document.querySelectorAll('.card');
+        cardBody = document.querySelectorAll('.card-body'),
+        cardchar = document.querySelectorAll('.card-char'),
+        card = document.querySelectorAll('.card');
 
-          console.log(links[0].parentNode.parentNode);
-
-          function hideSpecification(){
-            card.forEach(item =>{
-                if (item.dataset.value == 'spectitle'){
-                    item.classList.add('hide');
-                }
-                
-            });
-          }
-          function showSpecification(){
-            card.forEach(item =>{
-                if (item.dataset.value == 'spectitle'){
-                    item.classList.remove('hide');
-                }
-            });
-          }
-    
-    hideSpecification();
-
-    links.addEventListener('click', (e)=>{
-        if(e.target && e.target.dataset.value == 'speclink'){
-            card.forEach(item =>{
+    function hideSpecification(){
+        card.forEach(item =>{
+            if (item.dataset.value == 'spectitle'){
                 item.classList.add('hide');
-                showSpecification();
-            });
+            }
+        });
         }
 
+    function showSpecification(){
+    card.forEach(item =>{
+        if (item.dataset.value == 'spectitle'){
+            item.classList.remove('hide');
+        }
     });
-      
-});
+    }
 
+    hideSpecification();
+
+    for (let i = 0; i < cardBody.length; i++){
+        let self = cardBody[i];
+        self.addEventListener('click',(event)=>{
+            event.preventDefault();
+            console.log('click');
+        });
+    }
+
+});
