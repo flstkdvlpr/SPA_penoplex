6e4a1dac6ed685aba38de1e754e058f166652581
Artem <sartvas.ne@yahoo.com>
2022-07-21 15:25:51 +0300

    Получение харакетристик карточек

--- js/script.js
+++ js/script.js
@@ -3,34 +3,53 @@ window.addEventListener('DOMContentLoaded', ()=>{
     const links = document.querySelectorAll(".card-link"),
         cardBody = document.querySelectorAll('.card-body'),
         cardchar = document.querySelectorAll('.card-char'),
+        spicificationSide1 = document.querySelectorAll('.card-side-1'),
+        spicificationSide2 = document.querySelectorAll('.card-side-2'),
         card = document.querySelectorAll('.card');
 
-    function hideSpecification(){
-        card.forEach(item =>{
-            if (item.dataset.value == 'spectitle'){
-                item.classList.add('hide');
-            }
+    function hideItemSpecification(){
+        spicificationSide2.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show');
         });
-        }
-
-    function showSpecification(){
-    card.forEach(item =>{
-        if (item.dataset.value == 'spectitle'){
-            item.classList.remove('hide');
-        }
-    });
     }
 
-    hideSpecification();
+    function showItemSpecification(){
+        spicificationSide2.forEach(item =>{
+            item.classList.add('show');
+            item.classList.remove('hide');
+        });
+    }
+
+
+    function hideItemInformation(){
+        spicificationSide1.forEach(item =>{
+            item.classList.add('hide');
+            item.classList.remove('show');
+        });
+    }
+
+    function showItemInformation(){
+        spicificationSide1.forEach(item =>{
+            item.classList.add('show');
+            item.classList.remove('hide');
+        });
+    }
+
+  
+    hideItemSpecification();
+
 
     for (let i = 0; i < cardBody.length; i++){
-        let self = cardBody[i];
-        self.addEventListener('click',(event)=>{
+        cardBody[i].addEventListener('click',(event)=>{
             event.preventDefault();
             if (event.target && event.target.dataset.value == 'speclink'){
-                showSpecification();
+                showItemSpecification();
+                hideItemInformation();
             }
         });
     }
+
+    
 
 });
--- js/test.js
+++ js/test.js
@@ -1,7 +1,1 @@
-function count(num){
-    return this*num;
-}
-
-const dubles = count.bind(2);
-
-console.log(dubles(3));
+console.log('click');
