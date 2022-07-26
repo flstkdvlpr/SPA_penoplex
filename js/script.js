5863418c3bb61a041a35625f8260481ee8cfbe09
Artem <sartvas.ne@yahoo.com>
2022-07-26 16:59:47 +0300

    Использование классов

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@ window.addEventListener('DOMContentLoaded', ()=>{
         card = document.querySelectorAll('.card');
 
 
-    house.addEventListener('click', (e)=>{
+ /*    house.addEventListener('click', (e)=>{
         const target = e.target;
         if(target && target.dataset.item == 'roofpitched'){
             //hideAllItems();
             parent.document.location.href = "http://127.0.0.1:5500/index.html#catalog";
             console.log('click');
         }
-    });
+    }); */
 
 
     function hideAllItems(){
@@ -26,8 +26,9 @@ window.addEventListener('DOMContentLoaded', ()=>{
         item.addEventListener('click', (e)=>{
             e.preventDefault();
             if(e.target && e.target.dataset.value == 'speclink'){
-                item.childNodes[3].classList.remove('hide');
-                item.childNodes[1].classList.add('hide');
+                console.log(item.childNodes.childNodes);
+                //item.childNodes[3].classList.remove('hide');
+                //item.childNodes[1].classList.add('hide');
 
             }
         });
@@ -37,11 +38,109 @@ window.addEventListener('DOMContentLoaded', ()=>{
         item.addEventListener('click', (e)=>{
             e.preventDefault();
             if(e.target && e.target.dataset.value == 'specendlink'){
-                item.childNodes[1].classList.remove('hide');
-                item.childNodes[3].classList.add('hide');
-                console.log(item.childNodes[3]);
+                //item.childNodes[1].classList.remove('hide');
+                //item.childNodes[3].classList.add('hide');
+                //console.log(item.childNodes[3]);
             }
         });
     }); 
+
+    class MenuCardSide1{
+        constructor(side, img, title, text1, text2, text3, price, parentSelector){
+            this.side = side;
+            this.img = img;
+            this.title = title;
+            this.text1 = text1;
+            this.text2 = text2;
+            this.text3 = text3;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+        }
+
+        render(){
+            const element = document.createElement('div');
+            element.innerHTML = `<div class="card-side-1">
+            <img src=${this.img} class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${this.title}</h5>
+            <p class="card-text">${this.text1}</p>
+            <p class="card-text">${this.text2}</p>
+            <p class="card-text">${this.text3}</p>
+            <div class="card-char" data-value="speclink">Характеристики</div>
+                <div class="card-orders">
+                    <div class="card-price">${this.price}</div>
+                    <div class="btn btn-primary">Купить</div>
+                </div>   
+            </div>
+        </div>
+            `;
+
+            this.parent.append(card);
+        }
+    }
+
+    class MenuCardSide2{
+        constructor(side, text1, text2, text3, text4, text5, text6, text7, parentSelector){
+            this.side = side;
+            this.text1 = text1;
+            this.text2 = text2;
+            this.text3 = text3;
+            this.text4 = text4;
+            this.text5 = text5;
+            this.text6 = text6;
+            this.text7 = text7;
+            this.parent = document.querySelector(parentSelector);
+        }
+
+        render(){
+            const element = document.createElement('div');
+            element.innerHTML = `<div class="card-side-${2}">
+            <div class="card-body">
+                <h5 class="card-title" data-value="spectitle">Характеристики</h5>
+                <p class="card-text">${this.text1}</p>
+                <p class="card-text">${this.text2}</p>
+                <p class="card-text">${this.text3}</p>
+                <p class="card-text">${this.text4}</p>
+                <p class="card-text">${this.text5}</p>
+                <p class="card-text">${this.text6}</p>
+                <p class="card-text">${this.text7}</p>
+                  <ul>
+                      <li>низкая теплопроводность</li>
+                      <li>нулевое водопоглощение (однородная структура из герметичных ячеек);</li>
+                      <li>высокая прочность на сжатие и изгиб;</li>
+                      <li>долговечность;</li>
+                      
+                  </ul>
+                  <div class="card-char" data-value="specendlink">Назад</div>
+            </div>
+        </div>
+            `;
+
+            this.parent.append(element);
+        }
+    }
+
+    new MenuCardSide1(
+        1,
+        "/img/osnova.jpeg",
+        "Пеноплэкс Основа!®",
+        "Теплоизоляция внешних и внутренних ограждающих конструкций.",
+        "В продаже имеется материал с толщиной листа 20, 30, 40, 50, 100 мм.",
+        "Область применения: стены, цоколи и первые этажи конструкций.",
+        "240 ₽/лист",
+        ".card"
+    ).render();
+
+    new MenuCardSide2(
+        2,
+        "Производятся по ТУ 5767-006-54349294-2014",
+        "Водопоглощение за 24 часа: 0,4%",
+        "Группа горючести по 123-ФЗ: Г4",
+        "Коэффициент теплопроводности: 0,034 Вт/(м×°К)",
+        "Звукоизоляция перегородки: 41 дБ",
+        "Плотность: 27-35 кг/м3",
+        "Модуль упругости: 17 МПа",
+        ".card"
+    ).render();
 
 });
