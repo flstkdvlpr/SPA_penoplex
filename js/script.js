31e25fd18cca30fb6df7b08451d0b0753d242680
Artem <sartvas.ne@yahoo.com>
2022-08-18 15:37:41 +0300

    Начал реализовывать логику показа товара по типу утепляемой конструкции: готово всё + добавил новый материал.

--- js/script.js
+++ js/script.js
@@ -4,28 +4,60 @@ window.addEventListener('DOMContentLoaded', ()=>{
         card = document.querySelectorAll('.card-1'),
         dataCircle = document.querySelectorAll('[data-circle]');
 
-        console.log(card);
+        console.log(document.querySelector('#data-km'));
         //console.log(dataos);
 
         function hideAll(){
             card.forEach(item => {
-                item.classList.add('hide');
+                item.closest(".col").style.display = 'none';
             });
         }
 
         function showAll(){
             card.forEach(item => {
-                item.classList.remove('hide');
+                item.closest(".col").style.display = 'block';
             });
         }
 
 housePc.addEventListener('click', (e)=>{
-    if(e.target && e.target.closest(".grid-elem-2")){
-        showAll();
-        document.querySelector('#data-km').style.display = 'block';
-        document.querySelector('#data-os').style.display = 'block';
+    if(e.target && (e.target.closest(".grid-elem-2") || e.target.closest(".grid-elem-6"))){
         hideAll();
+        document.querySelector('#data-km').closest(".col").style.display = 'block';
+        document.querySelector('#data-os').closest(".col").style.display = 'block';
+        document.querySelector('#data-gl').closest(".col").style.display = 'block';
     }
+    if(e.target && e.target.closest(".grid-elem-3")){
+        showAll();
+        hideAll();
+        document.querySelector('#data-rf').closest(".col").style.display = 'block';
+        document.querySelector('#data-gl').closest(".col").style.display = 'block';
+    }
+
+    if(e.target && (e.target.closest(".grid-elem-4") || e.target.closest(".grid-elem-7-2") )){
+        showAll();
+        hideAll();
+        document.querySelector('#data-km').closest(".col").style.display = 'block';
+        document.querySelector('#data-os').closest(".col").style.display = 'block';
+        document.querySelector('#data-wl').closest(".col").style.display = 'block';
+        document.querySelector('#data-gl').closest(".col").style.display = 'block';
+    }
+    
+    if(e.target && (e.target.closest(".grid-elem-9") || e.target.closest(".grid-elem-7-1") )){
+        showAll();
+        hideAll();
+        document.querySelector('#data-fn').closest(".col").style.display = 'block';
+        document.querySelector('#data-ge').closest(".col").style.display = 'block';
+        document.querySelector('#data-gl').closest(".col").style.display = 'block';
+    }
+
+    if(e.target && e.target.closest(".grid-elem-10")){
+        showAll();
+        hideAll();
+        document.querySelector('#data-eko').closest(".col").style.display = 'block';
+    }
+
+    
+    
 });
 
 
