426c066a729dff539c4776acc9dde4e6f85cdcd0
Artem <sartvas.ne@yahoo.com>
2022-08-23 15:37:03 +0300

    закончил реализацию гамбургера для мобильных + закончил с логикой

--- js/script.js
+++ js/script.js
@@ -124,8 +124,22 @@ houseMob.addEventListener('click', (e)=>{
         });
     });
     
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.hamburger');
 
+    hamburger.addEventListener('click', ()=>{
+        menu.classList.toggle('menu_active');
+        hamburger.classList.toggle('hamburger_active');
+    });
 
+    menuItem.forEach(item =>{
+        item.addEventListener('click', ()=>{
+            menu.classList.toggle('menu_active');
+            hamburger.classList.toggle('hamburger_active');
+        });
+    });
+    
 
 /* 
     fetch('http://localhost:3000/offers')
