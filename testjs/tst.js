8c8a06052b1179c82f94409dbe13d62ffce549da
Artem <sartvas.ne@yahoo.com>
2022-08-23 14:28:24 +0300

    начинаю реализацию меню-гамбургера для мобильной версии

--- testjs/tst.js
+++ testjs/tst.js
@@ -1,23 +1,3 @@
-const arr = [1, 3, 4, 55, 6, 7, 38, 9];
+let string = "an apple a day keeps the doctor away";
 
-function search(arr, item){
-  arr.sort((a,b)=>a - b);
-  let start = 0;
-  let end = arr.length - 1;
-  let mid;
-  while(start <= end){
-    mid = Math.round((end - start)/2) + start;
-    if (item == arr[mid]){
-      return mid;
-    } else if (item < arr[mid]){
-      end = mid - 1;
-    } else {
-      start = mid + 1;
-    }
-  }
-   return -1;
-}
-
-
-console.log(search(arr, 55));
-
+console.log(string[0].toUpperCase);
