21e8442b5eff6aaac5f8e018dd51b5be0f349395
egor <artem.savinykh@hotmail.com>
2022-09-07 23:56:05 +0300

    Добавлены модули главного экрана

---
+++ src/components/header/header-hamburger/hamburger.js
@@ -0,0 +1,13 @@
+import './hamburger.css';
+
+const Hamburger = () => {
+    return (
+    <div class="hamburger">
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+    )
+}
+
+export default Hamburger;
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,14 @@
 import './header.css';
 import HeadeNav from './header-nav/header-nav';
+import Hamburger from './header-hamburger/hamburger';
+import HousePC from '../house-pc/house-pc';
 
 const Header = () =>{
     return(
         <header>
             <HeadeNav />
+            <Hamburger />
+            <HousePC />
         </header>
     )
 }
